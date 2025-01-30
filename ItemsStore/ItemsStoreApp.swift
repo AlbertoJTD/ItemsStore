f12101a3108d44511e4375a23f56c014fe5a3058
AlbertoJTD <albertojtd@outlook.com>
2025-01-30 17:55:06 -0600

    Initial Commit

---
+++ ItemsStore/ItemsStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItemsStoreApp.swift
+//  ItemsStore
+//
+//  Created by Alberto Jesus Tepale Diagas on 30/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ItemsStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ItemsStoreTests/ItemsStoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  ItemsStoreTests.swift
+//  ItemsStoreTests
+//
+//  Created by Alberto Jesus Tepale Diagas on 30/01/25.
+//
+
+import Testing
+@testable import ItemsStore
+
+struct ItemsStoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
